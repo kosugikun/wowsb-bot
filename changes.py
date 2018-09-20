3df11ec0fa27212a20028807a2632193efa827ca
kosugikun <kosukisaki@yahoo.co.jp>
2018-09-20 23:09:36 +0900

    0.1.3アップデート

--- changes.py
+++ changes.py
@@ -1,28 +1,8 @@
-# World of Warships Blitz Discord Bot(WoWsb Bot)
 # Copyright (C) 2018  WoWsb Japan community
-#
-# WoWsb Bot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WoWsb Bot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with WoWsb Bot. If not, see <http://www.gnu.org/licenses/>.
 import os.path
 
-CHANGES = """
-このアップデートでは、日本ツリー全てが実装されています。
-•日本ツリー駆逐艦を追加
-•日本ツリー戦艦を追加
-•日本ツリー巡洋艦を追加
-•日本ツリー航空母艦を追加
-•一部情報入力ミスを修正
-•その他細かな修正
+changes = """
+このバージョンでは、大日本帝国の新たな駆逐艦4隻とプレミアム艦1隻が追加されました。
+——————————————
+バグがあった場合サポートサーバーやGithubで教えてください。
 """
-
-UPIMAGE = "https://mcpenano.net/WoWsB/uplog/Version-0.1.2.png"
