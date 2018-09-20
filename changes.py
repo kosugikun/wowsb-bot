451d128ebdcc33760b2303ce95498181e3ddfa9c
kosugikun <kosukisaki@yahoo.co.jp>
2018-09-20 23:25:20 +0900

    バグ修正

--- changes.py
+++ changes.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2018  WoWsb Japan community
 import os.path
 
-changes = """
+CHANGES = """
 このバージョンでは、大日本帝国の新たな駆逐艦4隻とプレミアム艦1隻が追加されました。
 ——————————————
 バグがあった場合サポートサーバーやGithubで教えてください。
 """
+UPIMAGE = "https://mcpenano.net/WoWsB/uplog/Version-0.1.3.png"
